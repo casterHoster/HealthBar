f380b0f47d7b1b8da9f0441dd7a155b63b792ef8
casterHoster <95349741+casterHoster@users.noreply.github.com>
2024-03-26 21:49:28 +0300

    Revert "fucking shit"
    This reverts commit e33e299a8205a0ffa8cd7b8885625bdc8c1841c0.

--- HealthBar/Assets/Scripts/Health.cs
+++ HealthBar/Assets/Scripts/Health.cs
@@ -1,31 +1,21 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
-    private float _maxValue;
-    private float _currentValue;
+    [SerializeField] private float _maxHealth;
+    [SerializeField] private float _decreaseValue;
+    [SerializeField] private float _increaseValue;
 
-    public float MaxValue { get => _maxValue;}
-    public float CurrentValue { get => _currentValue;}
+    public event UnityAction<float> ChangeCount;
 
-    public event UnityAction<float, float> ChangedCount;
-
-    public void IncreaseValue(float value)
+    public void Increase()
     {
-        _currentValue += value;
-
-        if (_currentValue > _maxValue)
-        {
-            _currentValue = _maxValue;
-        }
-
-        ChangedCount?.Invoke(_currentValue, _maxValue);
+        ChangeCount?.Invoke(_increaseValue);
     }
 
-    public void DecreaseValue(float value)
+    public void Decrease()
     {
-        ChangedCount?.Invoke();
+        ChangeCount?.Invoke(_decreaseValue);
     }
 }
